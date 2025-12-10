987e6a5736e3e1c03eef0b13d191c5270997b85e
bondazz <miriyevsamir622@gmail.com>
2025-12-10 09:50:17 +0400

    Update page.tsx

--- src/app/(main)/add_job/page.tsx
+++ src/app/(main)/add_job/page.tsx
@@ -2,7 +2,7 @@ import ReferralJobSubmissionClient from '@/components/ReferralJobSubmissionClien
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'İş Elanı Yerləşdir | Namizəd Tapma Platforması – Jooble.az',
+    title: 'İş Elanı Yerləşdir | Namizəd Tapma Platforması ',
     description: 'Vakansiyanızı ən uyğun namizədlərə çatdırın. Jooble.az işə qəbul prosesinizi sürətləndirən peşəkar vakansiya yerləşdirmə və namizəd tapma platforması təqdim edir.',
     keywords: 'iş elanı yerləşdir, vakansiya yerləşdirmək, iş elanı vermək, namizəd tapma platforması, işəgötürən paneli, HR vakansiya sistemi, işə qəbul platforması, Azərbaycan vakansiyaları'
 };
