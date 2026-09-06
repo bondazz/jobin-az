802fec60a1ea273904ca0cdb0c951369a412c07a
Samir Miriyev <samir.m@Mjfinance.local>
2026-09-06 12:31:52 +0400

    Replace lucide_icons with lucide_icons_flutter to fix Flutter IconData build error

--- Jooble-app/lib/screens/home_screen.dart
+++ Jooble-app/lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../models/job.dart';
--- Jooble-app/lib/widgets/add_job_modal.dart
+++ Jooble-app/lib/widgets/add_job_modal.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
